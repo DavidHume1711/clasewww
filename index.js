2de04142e3d6bfbb13414e2e17bfc7fa93f57b9a
Juanes-xd <juanestebanortizbejarano@gmail.com>
2022-11-04 19:48:20 -0500

    endPoint Juanes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hola Felipe!');
+app.get("/", (req, res) => {
+  res.send("Hola Felipe!");
 });
 
-app.get('/endpoint1', (req, res) => {
-  res.send('Hola JuanD!');
+app.get("/endpoint1", (req, res) => {
+  res.send("Hola JuanD!");
+});
+
+app.get("/endpoint2", (req, res) => {
+  res.send("Hola Juanes!");
 });
 
 app.listen(port, () => {
